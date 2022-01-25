01419dc5ed00bd5dd382597736cd47e5841ff58c
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2022-01-25 21:49:46 +0530

    arithmetic operations

---
+++ Calculations.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Calculations
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int x = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int y = sc.nextInt();
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+        System.out.println(x % y);
+    }
+}
