79c026700fb14ebab71de090ba0f22107ef25ac8
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2022-01-25 22:32:53 +0530

    for each loop

---
+++ ForEach.java
@@ -0,0 +1,12 @@
+class ForEach
+{
+    public static void main(String[] args)
+    {
+        int[] numbers = {45, 30, 100, 37, 56};
+
+        for (int num : numbers)
+        {
+            System.out.println(num);
+        }
+    }
+}
