7af4a543e0f88578baaa5bcd2c9553870176c21b
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2022-01-25 21:56:27 +0530

    functions and functions overloading

--- School.java
+++ School.java
@@ -6,11 +6,18 @@ class School
         s1.name = "ABC";
         s1.rollno = 1;
         s1.course = "Java";
+        s1.calFees();
+        double s1Fees = s1.getFees();
 
         Student s2 = new Student();
         s2.name = "XYZ";
         s2.rollno = 2;
         s2.course = "Web";
+        s2.calFees(6);
+        double s2Fees = s2.getFees();
+
+        double totalFees = s1Fees + s2Fees;
+        System.out.println("Total fees to collect: " + totalFees);
     }
 }
 
@@ -20,4 +27,22 @@ class Student
     int rollno;
     String course;
     double fees;
+
+    void calFees(int months)
+    {
+        fees = 5000 * months;
+        System.out.println("Your fees is " + fees);
+    }
+
+    void calFees()
+    {
+        fees = 5000;
+        System.out.println("Your fees is " + fees);
+    }
+
+    double getFees()
+    {
+        return fees;
+    }
+
 }
