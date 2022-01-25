c9e0f428204fd667ca6b800e8142c90ded6ffa86
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2022-01-25 22:11:00 +0530

    loops

---
+++ MyLoops.java
@@ -0,0 +1,27 @@
+class MyLoops
+{
+    public static void main(String[] args)
+    {
+        // While Loop
+        int i = 0;
+        while (i < 11)
+        {
+            System.out.println(i);
+            i++;
+        }
+
+        // For Loops
+        for (int i = 0; i < 11; i++)
+        {
+            System.out.println(i);
+        }
+
+        // Do While Loop
+        int i = 10;
+        do
+        {
+            System.out.println(i);
+            i++;
+        } while (i < 5);
+    }
+}
