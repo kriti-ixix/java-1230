966d97c0da07ad1374fade48752f4802e382c055
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2022-01-27 13:24:26 +0530

    arraylist operations

--- MyArrayList.java
+++ MyArrayList.java
@@ -5,15 +5,23 @@ class MyArrayList
     public static void main(String[] args)
     {
         ArrayList<Integer> numbers = new ArrayList<>();
-        System.out.println(numbers.size());
+        // System.out.println(numbers.size());
 
         numbers.add(5);
         numbers.add(10);
         numbers.add(-100);
         numbers.add(50);
 
-        System.out.println(numbers.size());
-        System.out.println(numbers.get(2));
-        System.out.println(numbers.clear());
+        numbers.add(0, 10);
+        numbers.set(2, -20);
+
+        // System.out.println(numbers.size());
+        // System.out.println(numbers.get(2));
+        // System.out.println(numbers.clear());
+
+        for (int n: numbers)
+        {
+            System.out.println(n);
+        }
     }
 }
