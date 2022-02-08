97f8e41f8176e56d37f4d423d3859e4bb138fb5b
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2022-02-08 15:18:29 +0530

    interfaces

---
+++ MyInterfaces.java
@@ -0,0 +1,39 @@
+interface Mensuration
+{
+    void getArea();
+    void getPerimeter();
+}
+
+class Rectangle implements Mensuration
+{
+    public void getArea()
+    {
+
+    }
+
+    public void getPerimeter()
+    {
+
+    }
+}
+
+class Square implements Mensuration
+{
+    public void getArea()
+    {
+
+    }
+
+    public void getPerimeter()
+    {
+        
+    }
+}
+
+class MyInterfaces
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
